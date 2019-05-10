493fb31a954ed12213325594cb6c0238bb933266
Adina Bucur <adinaa.bucur@gmail.com>
2019-05-10 19:39:51 +0300

    studied one to one and many to many relationships final

--- src/main/java/org/fasttrackit/onlineshopapi/service/CustomerService.java
+++ src/main/java/org/fasttrackit/onlineshopapi/service/CustomerService.java
@@ -14,7 +14,9 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class CustomerService {
-    private static final Logger LOGGER = LoggerFactory.getLogger(CustomerService.class);
+
+    private static final Logger LOGGER =
+            LoggerFactory.getLogger(CustomerService.class);
 
     private final CustomerRepository customerRepository;
     private final ObjectMapper objectMapper;
@@ -31,29 +33,26 @@ public class CustomerService {
         return customerRepository.save(customer);
     }
 
-    public Customer getCustomer (long id) throws ResourceNotFoundException {
-        LOGGER.info("Retrieving customer{}", id);
+    public Customer getCustomer(long id) throws ResourceNotFoundException {
+        LOGGER.info("Retrieving customer {}", id);
         return customerRepository.findById(id)
-                //Optional and lambda expression
-                .orElseThrow(() -> new ResourceNotFoundException("Product " + id + " not found"));
-
+                // Optional and lambda expression
+                .orElseThrow(() -> new ResourceNotFoundException(
+                        "Customer " + id + " not found"));
     }
 
-    public Customer updateCustomer (long id, UpdateCustomerRequest request) throws ResourceNotFoundException {
-        LOGGER.info("Updating customer{}, {}", id, request);
+    public Customer updateCustomer(long id, UpdateCustomerRequest request) throws ResourceNotFoundException {
+        LOGGER.info("Updating customer {}, {}", id, request);
         Customer customer = getCustomer(id);
 
         BeanUtils.copyProperties(request, customer);
-        return customerRepository.save(customer);
 
+        return customerRepository.save(customer);
     }
 
-    public void deleteCustomer (long id){
+    public void deleteCustomer(long id) {
         LOGGER.info("Deleting customer {}", id);
         customerRepository.deleteById(id);
-        LOGGER.info("Deleting customer {}", id);
+        LOGGER.info("Deleted customer {}", id);
     }
 }
-
-
-
