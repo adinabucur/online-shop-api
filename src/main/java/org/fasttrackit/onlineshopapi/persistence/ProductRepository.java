61aa5bf1bcfafe769b1152bc26deb514cc4b6e32
Adina Bucur <adinaa.bucur@gmail.com>
2019-05-16 20:57:02 +0300

    added function to add products to cart

--- src/main/java/org/fasttrackit/onlineshopapi/persistence/ProductRepository.java
+++ src/main/java/org/fasttrackit/onlineshopapi/persistence/ProductRepository.java
@@ -3,24 +3,21 @@ package org.fasttrackit.onlineshopapi.persistence;
 import org.fasttrackit.onlineshopapi.domain.Product;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
-//Long is wrapper for primitive long
 public interface ProductRepository extends PagingAndSortingRepository<Product, Long> {
 
-    Page<Product> findByNameContainingAndQuantityGreaterThanEqual(
-            String partialName, int minimumQuantity, Pageable pageable);
+    Page<Product> findByNameContainingAndQuantityGreaterThanEqual(String partialName, int minimumQuantity, Pageable pageable);
 
-    Page<Product> findByPriceBetweenAndQuantityGreaterThanEqual(
-            double minimumPrice, double maximumPrice, int minimumQuantitity, Pageable pageable);
+    Page<Product> findByQuantityGreaterThanEqualAndPriceBetween(int minimumQuantity, double minimumPrice, double maximumPrice, Pageable pageable);
 
-    Page<Product> findByNameContainingAndPriceBetweenAndQuantityGreaterThanEqual(
-           String name, double minimumPrice, double maximumPrice, int minimumQuantitity, Pageable pageable);
+    Page<Product> findByNameContainingAndQuantityGreaterThanEqualAndPriceBetween(String partialName, int minimumQuantity, double minimumPrice, double maximumPrice, Pageable pageable);
 
     Page<Product> findByNameContaining(String partialName, Pageable pageable);
 
-    //same result as yhe method above (except the returned columns)
-   @Query(value = "SELECT id, name FROM Product product WHERE name LIKE '%?1'")
-   Page<Product> finByPartialName(String partialName, Pageable pageable);
+    //   same result as the method above (except the returned columns)
+//    @Query("SELECT id, name FROM  Product product WHERE name LIKE '%?1'")
+//    Page<Product> findByPartialName(String partialName, Pageable pageable);
+
+
 }
--- src/main/java/org/fasttrackit/onlineshopapi/service/ProductService.java
+++ src/main/java/org/fasttrackit/onlineshopapi/service/ProductService.java
@@ -6,20 +6,25 @@ import org.fasttrackit.onlineshopapi.exception.ResourceNotFoundException;
 import org.fasttrackit.onlineshopapi.persistence.ProductRepository;
 import org.fasttrackit.onlineshopapi.transfer.product.CreateProductRequest;
 import org.fasttrackit.onlineshopapi.transfer.product.GetProductsRequest;
+import org.fasttrackit.onlineshopapi.transfer.product.ProductResponse;
 import org.fasttrackit.onlineshopapi.transfer.product.UpdateProductRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.ListableBeanFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class ProductService {
 
-    private static final Logger LOGGER =
-            LoggerFactory.getLogger(ProductService.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(ProductService.class);
 
     private final ProductRepository productRepository;
     private final ObjectMapper objectMapper;
@@ -32,48 +37,63 @@ public class ProductService {
 
     public Product createProduct(CreateProductRequest request) {
         LOGGER.info("Creating product {}", request);
+
         Product product = objectMapper.convertValue(request, Product.class);
         return productRepository.save(product);
+
     }
 
     public Product getProduct(long id) throws ResourceNotFoundException {
         LOGGER.info("Retrieving product {}", id);
-        return productRepository.findById(id)
-                // Optional and lambda expression
-                .orElseThrow(() -> new ResourceNotFoundException(
-                        "Product " + id + " not found"));
+        //optional and lambda expression
+        return productRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Product" + id + " not found"));
+
     }
 
-
-
-    public Page<Product> getProducts(GetProductsRequest request, Pageable pageable) {
+    public Page<ProductResponse> getProducts(GetProductsRequest request, Pageable pageable) {
         LOGGER.info("Retrieving products >> {}", request);
+        Page<Product> products;
 
-        //not an elegant solution, but the easiest one for now
-
-        if (request.getPartialName() != null &&
-                request.getMinimumQuantity() != null &&
+        if (request.getPartialName() != null && request.getMinimumQuantity() != null &&
                 request.getMinimumPrice() != null &&
                 request.getMaximumPrice() != null) {
-            return productRepository.findByNameContainingAndPriceBetweenAndQuantityGreaterThanEqual(
-                    request.getPartialName(), request.getMinimumPrice(),
-                    request.getMaximumPrice(), request.getMinimumQuantity(), pageable);
+            products = productRepository.findByNameContainingAndQuantityGreaterThanEqualAndPriceBetween(
+                    request.getPartialName(), request.getMinimumQuantity(), request.getMinimumPrice(),
+                    request.getMaximumPrice(), pageable);
 
-        } else if (request.getMinimumPrice() != null &&
-                request.getMaximumPrice() != null &&
-                request.getMinimumQuantity() != null) {
-            return productRepository.findByPriceBetweenAndQuantityGreaterThanEqual(
+        } else if (request.getMinimumQuantity() != null &&
+                request.getMinimumPrice() != null &&
+                request.getMaximumPrice() != null) {
+            products = productRepository.findByQuantityGreaterThanEqualAndPriceBetween(request.getMinimumQuantity(),
                     request.getMinimumPrice(), request.getMaximumPrice(),
-                    request.getMinimumQuantity(), pageable);
+                    pageable);
 
         } else if (request.getPartialName() != null &&
                 request.getMinimumQuantity() != null) {
-            return productRepository.findByNameContainingAndQuantityGreaterThanEqual(
+            products = productRepository.findByNameContainingAndQuantityGreaterThanEqual(
                     request.getPartialName(), request.getMinimumQuantity(), pageable);
-        }
 
-        return productRepository.findAll(pageable);
+        } else if (request.getPartialName() != null) {
+            products = productRepository.findByNameContaining(request.getPartialName(), pageable);
+        } else {
+            products = productRepository.findAll(pageable);
+        }
+        List<ProductResponse> productResponses = new ArrayList<>();
+        for (Product product : products.getContent()) {
+            ProductResponse productResponse = new ProductResponse();
+            productResponse.setId(product.getId());
+            productResponse.setName(product.getName());
+            productResponse.setPrice(product.getPrice());
+            productResponse.setQuantity(product.getQuantity());
+            productResponse.setSku(product.getSku());
+
+            productResponses.add(productResponse);
+
+        }
+        return new PageImpl<>(productResponses, pageable, products.getTotalElements());
+
     }
+
 
     public Product updateProduct(long id, UpdateProductRequest request) throws ResourceNotFoundException {
         LOGGER.info("Updating product {}, {}", id, request);
@@ -87,6 +107,9 @@ public class ProductService {
     public void deleteProduct(long id) {
         LOGGER.info("Deleting product {}", id);
         productRepository.deleteById(id);
+
         LOGGER.info("Deleted product {}", id);
+
+
     }
 }
